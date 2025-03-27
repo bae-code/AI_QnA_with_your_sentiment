80873c84c92493e015d1a9881ac76c72accfe00f
bae-code <locing45@gmail.com>
2025-03-27 12:07:48 +0900

    ✨ 편지, 채팅 모델 구조

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,8 +1,27 @@
-from pydantic import BaseModel
+from datetime import datetime
+from pydantic import BaseModel, Field
+from uuid import uuid4
 
 
 class Chat(BaseModel):
-    id: str
+    id: str = Field(default_factory=lambda: str(uuid4()), alias="_id")
     user_id: str
+    title: str = Field(default="")
     content: str
-    
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+    is_archived: bool = False
+    archived_at: datetime | None = None
+    is_deleted: bool = False
+    deleted_at: datetime | None = None
+
+
+class Message(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid4()), alias="_id")
+    chat_id: str
+    pair_id: str
+    next_id: str
+    role: str
+    content: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
--- src/letter/models.py
+++ src/letter/models.py
@@ -4,23 +4,13 @@ from uuid import uuid4
 
 
 class LetterContent(BaseModel):
-    letter_id: str = Field(default_factory=lambda: str(uuid4()), alias="_id")
+    id: str = Field(default_factory=lambda: str(uuid4()), alias="_id")
     sender: str
     receiver: str
     content: str
-    is_read: bool
-    created_at: datetime
-    read_at: datetime
-
-class Letters(BaseModel):
-    id: str = Field(default_factory=lambda: str(uuid4()), alias="_id")
-    user_id: uuid4
-    status: str
-    created_at: datetime
-    updated_at: datetime
-    content_list: list[LetterContent]
-    
-
-
+    is_read: bool = False
+    created_at: datetime = Field(default_factory=datetime.now)
+    read_at: datetime | None = None
+    reply_to: str | None = None
 
 
