5a0870ef503a9b97841680a2193c83c67e63d3b7
bae-code <locing45@gmail.com>
2025-03-27 15:21:36 +0900

    🔧 pydantic 변경 수정

--- src/config.py
+++ src/config.py
@@ -1,12 +1,17 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
+
 
 class Settings(BaseSettings):
     MONGO_URI: str
     REDIS_URL: str
     MONGO_DB_NAME: str
+    # SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    ALGORITHM: str = "HS256"
 
-    class Config:
-        env_file = ".env"
-        
+    model_config = ConfigDict(env_file=".env")
+
 
 settings = Settings()
