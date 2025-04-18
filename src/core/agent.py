3805bc11164092ccdd8856c76623cf35d9492534
bae-code <locing45@gmail.com>
2025-04-18 21:16:14 +0900

    ✨ QA agent 및 MCP  추가

--- src/core/agent.py
+++ src/core/agent.py
@@ -1,6 +1,7 @@
 from agents.result import Agent
-from pydantic import BaseModel
 from agents.result import RunResult
+from agents import Runner
+from src.core.schema import ResponseQAResult
 
 
 class BaseAgent(Agent):
@@ -12,3 +13,38 @@ class BaseAgent(Agent):
 
     def _get_prompt(self, **kwargs):
         raise NotImplementedError("Subclasses must implement this method")
+
+
+class ResponseQAAgent(BaseAgent):
+    def __init__(self):
+        super().__init__(
+            name="Response QA Agent",
+            instructions="너는 에이전트의 응답이 전달된 프롬프트에 알맞은지 검토해줘. 프롬프트에 명시된 내용이 정확하지 않다고 판단되면 프롬프트에 명시된 형태로 수정해줘. 프롬프트에 명시된 내용이 정확하다고 판단되면 그대로 사용해. 너는 한국어로 대답해야해.",
+            output_type=ResponseQAResult,
+        )
+
+    async def run(self, prompt: str, response: str) -> dict:
+        """Check if the response aligns with the given prompt."""
+        check_prompt = self._get_prompt(prompt, response)
+        result = await Runner.run(self, input=check_prompt)
+        return self._validate_result(result=result)
+
+    def _get_prompt(self, prompt: str, response: str) -> str:
+        return f"""
+        [프롬프트]
+        {prompt}
+
+        [응답]
+        {response}
+
+        위의 응답이 프롬프트의 지시사항에 맞게 작성되었는지 확인해주세요.
+        - 맞으면: "PASS"를 출력하고 응답 그대로 사용하세요.
+        - 틀렸으면: "FAIL"을 출력하고, 프롬프트에 맞게 응답을 수정본과 위반사항을 포함하여 함께 출력해주세요.
+        결과는 반드시 다음 JSON 포맷으로 출력하세요:
+
+        {{
+            "status": "PASS" | "FAIL",
+            "corrected_response": "..."  // PASS인 경우에도 원본 응답 다시 넣어주세요
+            "violation": "..."  // 위반사항을 포함하여 함께 출력해주세요.
+        }}
+        """
---
+++ src/mcp_test.py
@@ -0,0 +1,62 @@
+import sys
+import httpx
+
+from mcp.server.fastmcp import FastMCP
+from config import settings
+
+# Initialize FastMCP server
+mcp = FastMCP("weather")
+
+# Constants
+NWS_API_BASE = "https://api.weather.gov"
+USER_AGENT = "weather-app/1.0"
+
+
+accu_api_key = settings.ACCU_WEATHER_API_KEY
+accu_base_url = "http://dataservice.accuweather.com/"
+
+
+@mcp.tool(
+    name="get_city_location_info",
+    description="도시 이름을 입력하여 도시 위치정보를 반환하는 도구, 도시의 이름은 한글로 입력해야합니다.",
+)
+async def get_city_location_info(city_name: str):
+    url = f"{accu_base_url}/locations/v1/cities/search"
+    params = {
+        "apikey": accu_api_key,
+        "q": city_name,
+        "language": "ko-kr",
+        "details": "true",
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=params, timeout=30.0)
+        response.raise_for_status()
+        city_info = response.json()
+        location_key = city_info[0].get("Details").get("CanonicalLocationKey")
+        return location_key
+
+
+@mcp.tool(
+    name="get_forecast",
+    description="위치정보와 원하는 날짜값을 입력하면 날씨 정보를 반환해줘, 날짜는 string 타입의 1,5,10,15 만 가능하고 숫자가 아니라면 가장 가까운 숫자를 찾아서 쓰세요.",
+)
+async def get_forecast(location_key: str, days: str):
+    days = days + "day"
+    url = f"{accu_base_url}/forecasts/v1/daily/{days}/{location_key}"
+    params = {
+        "apikey": accu_api_key,
+        "locationKey": location_key,
+        "language": "ko-kr",
+        "details": "true",
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=params, timeout=30.0)
+        response.raise_for_status()
+        return response.json()
+
+
+if __name__ == "__main__":
+    print("✅ MCP 서버 시작됨 (stdio 모드)", file=sys.stderr)
+    # Initialize and run the server
+    mcp.run(transport="stdio")
